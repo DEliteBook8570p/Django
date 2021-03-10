ea6f2b544b717589cd7676407475d2e425aefe68
delvina <d.blakaj1108@gmail>
2021-03-10 22:56:36 +0100

    Runtime.txt added

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'delvblakaj1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pasidel1*1'
+EMAIL_HOST_USER = 'EMAIL_USER'
+EMAIL_HOST_PASSWORD = 'EMAIL_PASS'
 
